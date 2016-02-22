485839773b5a98213e539696a3a0c776dcbb7ac9
Marius Petroaie <marius.petroaie@gmail.com>
2016-02-22 06:01:54 +0000

    adjust the homepage copy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="jumbotron">
-                <h1>Hello, world!</h1>
-                <p>...</p>
-                <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="jumbotron">
+                    <h1>hey!</h1>
+                    <p>...</p>
+                    <p>
+                        @if ($loggedInUserIsAdmin)
+                            <a class="btn btn-primary btn-lg"
+                               href="{{ url('/users') }}"
+                               role="button"
+                            >
+                                Manage Users
+                            </a>
+                        @endif
+                        <a class="btn btn-success btn-lg"
+                           href="{{ url('/users/' . $loggedInUserId . '/timesheets') }}"
+                           role="button"
+                        >
+                            My Timesheets
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
